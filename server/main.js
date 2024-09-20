e9b77f0cde5ba572f106ac5485559de5d2a09e92
Kaloyan Marinov <happy-2024-01-02-16-53@protonmail.com>
2024-09-20 23:10:35 +0200

    2: change `server/main.js` into inserting some sample data into the `TasksCollection`

--- server/main.js
+++ server/main.js
@@ -1,37 +1,22 @@
-import { Meteor } from 'meteor/meteor';
-import { LinksCollection } from '/imports/api/links';
+import { Meteor } from "meteor/meteor";
+import { TasksCollection } from "/imports/api/TasksCollection";
 
-async function insertLink({ title, url }) {
-  await LinksCollection.insertAsync({ title, url, createdAt: new Date() });
-}
+const insertTask = (taskText) => {
+  TasksCollection.insertAsync({
+    text: taskText,
+  });
+};
 
 Meteor.startup(async () => {
-  // If the Links collection is empty, add some data.
-  if (await LinksCollection.find().countAsync() === 0) {
-    await insertLink({
-      title: 'Do the Tutorial',
-      url: 'https://www.meteor.com/tutorials/react/creating-an-app',
-    });
-
-    await insertLink({
-      title: 'Follow the Guide',
-      url: 'https://guide.meteor.com',
-    });
-
-    await insertLink({
-      title: 'Read the Docs',
-      url: 'https://docs.meteor.com',
-    });
-
-    await insertLink({
-      title: 'Discussions',
-      url: 'https://forums.meteor.com',
-    });
+  if ((await TasksCollection.find().countAsync()) === 0) {
+    [
+      "Task 1",
+      "Task 2",
+      "Task 3",
+      "Task 4",
+      "Task 5",
+      "Task 6",
+      "Task 7",
+    ].forEach(insertTask);
   }
-
-  // We publish the entire Links collection to all clients.
-  // In order to be fetched in real-time to the clients
-  Meteor.publish("links", function () {
-    return LinksCollection.find();
-  });
 });
