60d46e993336030bd0f0a00203d2a222b71d7309
Kaloyan Marinov <happy-2024-01-02-16-53@protonmail.com>
2024-09-20 22:53:19 +0200

    1: hardcode an array of `tasks`

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
-import { Hello } from './Hello.jsx';
-import { Info } from './Info.jsx';
+import React from "react";
+import { Hello } from "./Hello.jsx";
+import { Info } from "./Info.jsx";
+
+const tasks = [
+  { _id: 1, text: "buy groceries" },
+  { _id: 2, text: "cook dinner" },
+  { _id: 3, text: "do the dishes" },
+];
 
 export const App = () => (
   <div>
     <h1>Welcome to Meteor!</h1>
-    <Hello/>
-    <Info/>
+    <Hello />
+    <Info />
   </div>
 );
