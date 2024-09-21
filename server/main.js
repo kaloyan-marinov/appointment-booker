a5efe4aa41569ce97866e23762977f6d60660050
Kaloyan Marinov <happy-2024-01-02-16-53@protonmail.com>
2024-09-21 14:19:19 +0200

    7: augment the `Meteor.startup(async () => { ...  })` into checking for a default user in the MongoDB and, if not found, creating said user

--- server/main.js
+++ server/main.js
@@ -1,4 +1,5 @@
 import { Meteor } from "meteor/meteor";
+import { Accounts } from "meteor/accounts-base";
 import { TasksCollection } from "/imports/api/TasksCollection";
 import "../imports/api/TasksPublications";
 import "../imports/api/tasksMethods";
@@ -10,7 +11,23 @@ const insertTask = (taskText) => {
   });
 };
 
+// TODO: (2024/09/21, 14:17)
+//      avoid hardcoding the following values
+const SEED_USERNAME = "meteorite";
+const SEED_PASSWORD = "password";
+
 Meteor.startup(async () => {
+  // TODO: (2024/09/21, 14:18)
+  //      look into why VS Code issues a warning for each of the next 2 uses of `await`,
+  //      with said warning reading
+  //      "'await' has no effect on the type of this expression.ts(80007)"
+  if (!(await Accounts.findUserByUsername(SEED_USERNAME))) {
+    await Accounts.createUser({
+      username: SEED_USERNAME,
+      password: SEED_PASSWORD,
+    });
+  }
+
   if ((await TasksCollection.find().countAsync()) === 0) {
     [
       "Task 1",
