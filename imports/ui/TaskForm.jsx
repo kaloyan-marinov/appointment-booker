76285db8abb0cbc08e9491506d9069b9f25af8a4
Kaloyan Marinov <happy-2024-01-02-16-53@protonmail.com>
2024-09-21 09:16:33 +0200

    3: add `imports/api/tasksMethods.js` (noting that «Meteor methods» are essentially Remote Procedure Calls (RPCs) to the server, which let you perform operations on the server side securely) + invoke the `tasks.insert` Meteor method from within `<TaskForm>` via a newly-created `onSubmit` handler

--- imports/ui/TaskForm.jsx
+++ imports/ui/TaskForm.jsx
@@ -1,10 +1,24 @@
 import React, { useState } from "react";
+import { TasksCollection } from "/imports/api/TasksCollection";
 
 export const TaskForm = () => {
   const [text, setText] = useState("");
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!text) return;
+
+    await Meteor.callAsync("tasks.insert", {
+      text: text.trim(),
+      createdAt: new Date(),
+    });
+
+    setText("");
+  };
+
   return (
-    <form className="task-form">
+    <form className="task-form" onSubmit={handleSubmit}>
       <input
         type="text"
         placeholder="Type to add new tasks"
