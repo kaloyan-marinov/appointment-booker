fd26f9e0d6185fd543e2c9c0213296a01be63e26
Kaloyan Marinov <happy-2024-01-02-16-53@protonmail.com>
2024-09-21 09:03:42 +0200

    3: call the `React.useState` hook in `<TaskForm>` + endow the `<input>` with an `onChange` handler

--- imports/ui/TaskForm.jsx
+++ imports/ui/TaskForm.jsx
@@ -1,11 +1,16 @@
 import React, { useState } from "react";
 
 export const TaskForm = () => {
-  //   const [text, setText] = useState("");
+  const [text, setText] = useState("");
 
   return (
     <form className="task-form">
-      <input type="text" placeholder="Type to add new tasks" />
+      <input
+        type="text"
+        placeholder="Type to add new tasks"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
 
       <button type="submit">Add Task</button>
     </form>
